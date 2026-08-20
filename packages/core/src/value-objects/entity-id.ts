f582114d5fe73b88d5f90414efc7c7f8516563dc
MAD1IZJOD <175943252+MAD1IZJOD@users.noreply.github.com>
2026-08-20 21:32:33 +0530

    changed entity idts

--- packages/core/src/value-objects/entity-id.ts
+++ packages/core/src/value-objects/entity-id.ts
@@ -4,6 +4,10 @@ export type EntityId = string & {
   readonly __brand: "EntityId";
 };
 
-export function createEntityId(): EntityId {
-  return randomUUID() as EntityId;
+export function createEntityId<T extends string = "EntityId">(): string & {
+  readonly __brand: T;
+} {
+  return randomUUID() as string & {
+    readonly __brand: T;
+  };
 }
