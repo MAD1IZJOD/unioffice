21fe0c41288ba9b341e8f9ea561e5ad96bb87290
MAD1IZJOD <175943252+MAD1IZJOD@users.noreply.github.com>
2026-08-26 19:19:05 +0530

    main file errors resolved

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import {
   RouterProvider,
 } from "react-router-dom";
 
 import { router } from "./app/routes";
 import "./index.css";
+import "./App.css";
 
 ReactDOM.createRoot(
   document.getElementById("root")!,
 ).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider
+      router={router}
+    />
   </React.StrictMode>,
 );
