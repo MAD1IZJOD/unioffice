a169e999b1ee11e63a1f1376fe955373a62707eb
MAD1IZJOD <175943252+MAD1IZJOD@users.noreply.github.com>
2026-08-28 21:22:10 +0530

    exporting planner

--- packages/orchestrator/src/index.ts
+++ packages/orchestrator/src/index.ts
@@ -1,4 +1,6 @@
 export * from "./planner/planner.js";
+export * from "./planner/ollama-planner.js";
+
 export * from "./delegation/delegator.js";
 export * from "./execution/execution-engine.js";
 export * from "./execution/work-plan-to-workflow.js";
